d06cce3429ae74c7dcd6093ca95be265b82c69bb
Erick Amoedo <erick.amoedo@live.com>
2022-04-03 18:15:54 -0300

    update - Depedencies fixed. Title now working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,24 @@
 import styles from '../styles/home.module.scss'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello World
-      </h1>
+
+    <>
+        <Head>
+        <title>Teste</title>
+          <meta
+            name="description"
+            content="Meta description for the About page"
+          />
+        </Head>
+
+        <h1 className={styles.title}>
+          Hello World
+        </h1>
+    
+
+      </>
+      
   )
 }
