bdabee92cd8330104bc4d6d7d7dce1d20c1bc171
Erick Amoedo <erick.amoedo@live.com>
2022-04-04 17:59:27 -0300

    Component Button

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,9 +1,20 @@
+import styles from "./styles.module.scss";
 import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
 
 export function SignInButton() {
-  return (
-    <button type="button">
-      <FaGithub />
+  const isUserLoggedIn = true;
+
+  //verifica se o usuario está logado para trocar o Icon. Verificação com um IF
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      Erick Amoedo
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417" />
       Entrar com GitHub
     </button>
   );
